f50f8ca675f6462eded17fcd71967ba05f56d719
SEKI-YUTA <zard.since1991@gmail.com>
2021-03-04 17:53:25 +0900

    anim change & font(sanserif) & MV height fixed

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 
 $(window).scroll(function (){
-  $('.el_fadeLeft').each(function(){
+  $('.el_fadeBottom').each(function(){
       var elemPos = $(this).offset().top,
           scroll = $(window).scrollTop(),
           windowHeight = $(window).height();
           console.log(scroll);
-        if (scroll > elemPos - windowHeight + 300){
-            $(this).addClass('el_scrollIn');
-          }
-      });
-  $('.el_fadeRight').each(function(){
-      var elemPos = $(this).offset().top,
-          scroll = $(window).scrollTop(),
-          windowHeight = $(window).height();
-          console.log(scroll);
-        if (scroll > elemPos - windowHeight + 300){
+        if (scroll > elemPos - windowHeight + 50){
             $(this).addClass('el_scrollIn');
           }
       });
