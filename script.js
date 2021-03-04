7784a8dca1ea0ec7b5a2b1015dc00b0886d1f210
SEKI-YUTA <zard.since1991@gmail.com>
2021-03-04 12:18:12 +0900

    fb fixed

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 
+$(window).scroll(function (){
+  $('.el_fadeLeft').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+          console.log(scroll);
+        if (scroll > elemPos - windowHeight + 300){
+            $(this).addClass('el_scrollIn');
+          }
+      });
+  $('.el_fadeRight').each(function(){
+      var elemPos = $(this).offset().top,
+          scroll = $(window).scrollTop(),
+          windowHeight = $(window).height();
+          console.log(scroll);
+        if (scroll > elemPos - windowHeight + 300){
+            $(this).addClass('el_scrollIn');
+          }
+      });
+  });
